a74c1778648e7413b580a12ba37137134ecb076d
Renaud Rohlinger <renaud.rohlinger@gmail.com>
2023-07-18 21:38:51 +0900

    copy dist for prod temp

--- scripts/copyBuild.js
+++ scripts/copyBuild.js
@@ -2,27 +2,17 @@ import { fileURLToPath } from 'url';
 import path from 'path';
 import fs from 'fs';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
-const src = path.resolve(__dirname, '../dist/main.js');
-const dest = path.resolve(__dirname, '../demo/main.js');
-
-fs.copyFile(src, dest, (err) => {
-  if (err) {
-    console.error('Error occurred while copying the file.', err);
-    process.exit(1);
-  }
-  console.log('main.es.js has been copied to the demo directory.');
-});
-
-const src2 = path.resolve(__dirname, '../dist/panel.js');
-const dest2 = path.resolve(__dirname, '../demo/panel.js');
-
-fs.copyFile(src2, dest2, (err) => {
-  if (err) {
-    console.error('Error occurred while copying the file.', err);
-    process.exit(1);
-  }
-  console.log('panel.es.js has been copied to the demo directory.');
-});
+// copy dir dist to demo/dist
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const demoDist = path.resolve(__dirname, '../demo/dist');
+const dist = path.resolve(__dirname, '../dist');
+const copyDir = (src, dest) => {
+    fs.mkdirSync(dest, { recursive: true });
+    fs.readdirSync(src).forEach((file) => {
+        const srcFile = path.join(src, file);
+        const destFile = path.join(dest, file);
+        fs.copyFileSync(srcFile, destFile);
+    });
+    }
+copyDir(dist, demoDist);
